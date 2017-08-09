e5fc8339e6a613ae775a00069b678c9ea4883e07
suriyanarayanank <ksuriyan@in.ibm.com>
2017-08-09 21:07:34 +0530

    Feat: Create storage folders Create storage folders and include permission for writing to external storage.

--- app/src/main/java/com/flipcam/PermissionActivity.java
+++ app/src/main/java/com/flipcam/PermissionActivity.java
@@ -13,16 +13,20 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.WindowManager;
 
 public class PermissionActivity extends AppCompatActivity {
 
     final String TAG = "PermissionActivity";
     private int MY_PERMISSIONS_REQUEST_CAMERA = 0;
     private int MY_PERMISSIONS_REQUEST_AUDIO = 1;
+    private int MY_PERMISSIONS_WRITE_STORAGE = 2;
     static final String AUDIO_PERMISSION = "android.permission.RECORD_AUDIO";
     static final String CAMERA_PERMISSION = "android.permission.CAMERA";
+    static final String STORAGE_PERMISSIONS = "android.permission.WRITE_EXTERNAL_STORAGE";
     boolean cameraPermission = false;
     boolean audioPermission = false;
+    boolean storagePermission = false;
     boolean showMessage = false;
     DialogInterface.OnClickListener exitListener;
     AlertDialog.Builder alertDialog;
@@ -49,6 +53,21 @@ public class PermissionActivity extends AppCompatActivity {
                 if (permissions[0].equalsIgnoreCase(AUDIO_PERMISSION)) {
                     if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                         audioPermission = true;
+                        checkExternalStoragePermission();
+                        //openCameraFragment();
+                    } else {
+                        quitFlipCam();
+                    }
+                }
+            } else {
+                super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+            }
+        } else if (requestCode == MY_PERMISSIONS_WRITE_STORAGE) {
+            if (permissions != null && permissions.length > 0) {
+                Log.d(TAG, "For audio == "+permissions[0]);
+                if (permissions[0].equalsIgnoreCase(STORAGE_PERMISSIONS)) {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        storagePermission = true;
                         openCameraFragment();
                     } else {
                         quitFlipCam();
@@ -70,6 +89,8 @@ public class PermissionActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean("startCamera",false);
         editor.commit();
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if(savedInstanceState!=null){
             if(savedInstanceState.getBoolean("restart")){
                 showMessage = true;
@@ -125,11 +146,9 @@ public class PermissionActivity extends AppCompatActivity {
             SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putBoolean("startCamera", true);
-            //editor.putBoolean("permissions",true);
             editor.commit();
             Intent cameraIntent = new Intent(this, CameraActivity.class);
             startActivity(cameraIntent);
-            //Toast.makeText(getApplicationContext(), "ALL Permissions in place. Need to open camera now.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -138,12 +157,31 @@ public class PermissionActivity extends AppCompatActivity {
         int permission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
         if (permission == PackageManager.PERMISSION_GRANTED) {
             audioPermission = true;
-            openCameraFragment();
+            //openCameraFragment();
         } else {
             Log.d(TAG, "Audio permission not obtained. Obtain explicitly");
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.RECORD_AUDIO},
                     MY_PERMISSIONS_REQUEST_AUDIO);
+        }
+        if(audioPermission){
+            checkExternalStoragePermission();
+        }
+    }
+
+    void checkExternalStoragePermission()
+    {
+        int permission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            storagePermission = true;
+        } else {
+            Log.d(TAG, "Storage permission not obtained. Obtain explicitly");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_WRITE_STORAGE);
+        }
+        if(storagePermission){
+            openCameraFragment();
         }
     }
 
--- app/src/main/java/com/flipcam/VideoFragment.java
+++ app/src/main/java/com/flipcam/VideoFragment.java
@@ -1,33 +1,21 @@
 package com.flipcam;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import java.io.File;
-
-import static android.os.Environment.getExternalStoragePublicDirectory;
 
 
 public class VideoFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
     public static final String TAG = "VideoFragment";
-    private int MY_PERMISSIONS_WRITE_STORAGE = 0;
-    private static final String STORAGE_PERMISSIONS = "android.permission.WRITE_EXTERNAL_STORAGE";
     public VideoFragment() {
         // Required empty public constructor
     }
@@ -50,58 +38,18 @@ public class VideoFragment extends Fragment {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == MY_PERMISSIONS_WRITE_STORAGE) {
-            if (permissions != null && permissions.length > 0) {
-                Log.d(TAG, "For storage == " + permissions[0]);
-                if (permissions[0].equalsIgnoreCase(STORAGE_PERMISSIONS)) {
-                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        fetchMedia();
-                    }
-                    else{
-                        Toast.makeText(getContext(),"FlipCam needs permission to store the video in the Gallery , so that you can play them later." +
-                                "If you do not wish to give permission, the files will be stored internally.",Toast.LENGTH_LONG).show();
-                    }
-                }
-            } else {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            }
-        }
-    }
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_video, container, false);
         Log.d(TAG,"Inside video fragment");
-        File file = getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
-        int permission = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        Log.d(TAG,"permission == "+permission);
-        if(permission != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(getActivity(),new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_WRITE_STORAGE);
-        }
-        else{
-            fetchMedia();
-        }
+        fetchMedia();
         return view;
     }
 
     private void fetchMedia()
     {
-        String[] projection = new String[] {
-                MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
-                MediaStore.Images.Media.DATE_TAKEN,
-                MediaStore.Images.Media.DISPLAY_NAME
-        };
-        String[] vid_projection = new String[]{
-            MediaStore.Video.Media._ID,
-            MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
-            MediaStore.Video.Media.DATE_TAKEN,
-            MediaStore.Video.Media.DISPLAY_NAME
-        };
-        File root = Environment.getExternalStorageDirectory();
+        File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
         Log.d(TAG,root.getPath());
         File fc = new File(root.getPath()+"/FlipCam");
         if(!fc.exists()){
@@ -118,44 +66,15 @@ public class VideoFragment extends Fragment {
                 Log.d(TAG,"Images dir created");
             }
         }
-        // content:// style URI for the "primary" external storage volume
-        //Uri images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        //This code is necessary when we want to display all media created by FlipCam, when user clicks on a widget.
-        Uri videos = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-        Log.d(TAG,videos+"");
-
-        Cursor cur = getContext().getContentResolver().query(videos,
-                vid_projection, // Which columns to return
-                MediaStore.Video.Media.BUCKET_DISPLAY_NAME+" = 'FC_Videos'",       // Which rows to return (all rows)
-                null,       // Selection arguments (none)
-                MediaStore.Images.Media.DATE_TAKEN + " DESC"        // Ordering
-        );
-
-        Log.i("ListingImages"," query count=" + cur.getCount());
-
-        if (cur.moveToFirst()) {
-            String bucket;
-            String date;
-            int bucketColumn = cur.getColumnIndex(
-                    MediaStore.Video.Media.BUCKET_DISPLAY_NAME);
-
-            int dateColumn = cur.getColumnIndex(
-                    MediaStore.Video.Media.DATE_TAKEN);
-
-            int id = cur.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME);
-
-            do {
-                // Get the field values
-                bucket = cur.getString(bucketColumn);
-                Log.d(TAG,bucket);
-                if(bucket.equalsIgnoreCase("FC_Videos")) {
-                    date = cur.getString(dateColumn);
-                    // Do something with the values.
-                    Log.i("ListingImages", " bucket=" + bucket
-                            + "  date_taken=" + date + " id = " + cur.getString(id));
-                }
-            } while (cur.moveToNext());
-
+        //Use this for sharing files between apps
+        File videos = new File(root.getPath()+"/FlipCam/FC_Videos/");
+        if(videos.listFiles() != null){
+            Log.d(TAG,"List = "+videos.listFiles());
+            Log.d(TAG,"List length = "+videos.listFiles().length);
+            for(File file : videos.listFiles()){
+                Log.d(TAG,file.getPath());
+                //Picasso.with(getContext())
+            }
         }
     }
 
