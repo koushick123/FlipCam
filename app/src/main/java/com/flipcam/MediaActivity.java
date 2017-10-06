4a01e511d13c7e33c595d5869c50ac53966acae4
koushick <skoushicksuri@gmail.com>
2017-10-06 05:43:45 +0530

    Fix: Align recorded video to play as per recorded orientation. Align recorded video to play as per recorded orientation.

--- app/src/main/java/com/flipcam/MediaActivity.java
+++ app/src/main/java/com/flipcam/MediaActivity.java
@@ -1,31 +1,35 @@
 package com.flipcam;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Gravity;
+import android.view.Display;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.VideoView;
 
 public class MediaActivity extends AppCompatActivity {
 
     private static final String TAG = "MediaActivity";
-    LinearLayout mediaPlaceholder;
+    RelativeLayout mediaPlaceholder;
     VideoView videoView=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
-        mediaPlaceholder = (LinearLayout)findViewById(R.id.mediaPlaceholder);
+        mediaPlaceholder = (RelativeLayout)findViewById(R.id.mediaPlaceholder);
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         getSupportActionBar().hide();
@@ -37,9 +41,6 @@ public class MediaActivity extends AppCompatActivity {
             VideoView videoView = (VideoView)findViewById(R.id.recordedVideo);
             mediaPlaceholder.removeView(videoView);
             ImageView picture = new ImageView(this);
-            LinearLayout.LayoutParams picParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            picParam.gravity = Gravity.CENTER;
-            picture.setLayoutParams(picParam);
             Log.d(TAG, "image path = " + intent.getStringExtra("mediaPath"));
             Bitmap image = BitmapFactory.decodeFile(intent.getStringExtra("mediaPath"));
             picture.setImageBitmap(image);
@@ -51,6 +52,31 @@ public class MediaActivity extends AppCompatActivity {
             videoView.setVisibility(View.VISIBLE);
             videoView.setKeepScreenOn(true);
             videoView.setVideoPath("file://"+path);
+            MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+            mediaMetadataRetriever.setDataSource(path);
+            String width = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
+            String height = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
+            Log.d(TAG,"Video Width / Height = "+width+" / "+height);
+            Log.d(TAG,"Aspect Ratio ==== "+Double.parseDouble(width)/Double.parseDouble(height));
+            double videoAR = Double.parseDouble(width)/Double.parseDouble(height);
+            WindowManager windowManager = (WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+            Display display = windowManager.getDefaultDisplay();
+            Log.d(TAG,"Rotation = "+display.getRotation());
+            Point screenSize=new Point();
+            display.getRealSize(screenSize);
+            double screenAR = (double)screenSize.x/(double)screenSize.y;
+            Log.d(TAG,"screen width / height= "+screenSize.x+" / "+screenSize.y);
+            Log.d(TAG,"Screen AR = "+screenAR);
+            if(display.getRotation() == Surface.ROTATION_0){
+                if(Math.abs(screenAR-videoAR) < 0.1) {
+                    adjustVideoToFitScreen();
+                }
+            }
+            else if(display.getRotation() == Surface.ROTATION_90 || display.getRotation() == Surface.ROTATION_270){
+                if(Math.abs(screenAR-videoAR) < 0.1) {
+                    adjustVideoToFitScreen();
+                }
+            }
             MediaController mediaController = new MediaController(this);
             videoView.setMediaController(mediaController);
             mediaController.show();
@@ -58,6 +84,15 @@ public class MediaActivity extends AppCompatActivity {
                 videoView.start();
             }
         }
+    }
+
+    public void adjustVideoToFitScreen(){
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        videoView.setLayoutParams(layoutParams);
     }
 
     @Override
