54c4989013c6468c3b57f74b16a79d37c6dad0c0
suriyanarayanank <ksuriyan@in.ibm.com>
2017-11-14 18:48:46 +0530

    Fix: Bug fixes 1) Scroll through media (video/image) without memory issues. 2) Use Glide to manage memory while rotating and redrawing images. 3) Add FlipCamGlideModule for use of Glide.

---
+++ app/src/main/java/com/flipcam/FlipCamGlideModule.java
@@ -0,0 +1,12 @@
+package com.flipcam;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by koushick on 14-Nov-17.
+ */
+
+@GlideModule
+public class FlipCamGlideModule extends AppGlideModule {
+}
