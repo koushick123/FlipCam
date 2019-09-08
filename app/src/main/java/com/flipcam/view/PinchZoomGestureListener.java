665767acaf088c7b9b8c6f5095af8ebd7dee80e2
Koushick Suriyanarayanan <skoushicksuri@gmail.com>
2019-09-09 04:51:47 +0530

    Fix: Pinch zoom feature Pinch zoom feature work in progress

--- app/src/main/java/com/flipcam/view/PinchZoomGestureListener.java
+++ app/src/main/java/com/flipcam/view/PinchZoomGestureListener.java
@@ -15,11 +15,12 @@ public class PinchZoomGestureListener extends ScaleGestureDetector.SimpleOnScale
     CameraView cameraView;
     Context appContext;
     float progress = 0.0f;
-    float progressStep = 1.5f;
+    float progressStep = 0.7f;
     Boolean isSmoothZoom = null;
     int cameraMaxZoom = -1;
+    float scaleSensitivity = 2.0f;
     int zoomLevel = -1;
-    //Set max zoom in restriction to 5.0x level
+    //Set max zoom in restriction to 50% of max level
     int restrictedMaxZoom = -1;
     public PinchZoomGestureListener(Context context, VideoFragment vFrag, PhotoFragment pFrag){
         appContext = context;
@@ -68,21 +69,30 @@ public class PinchZoomGestureListener extends ScaleGestureDetector.SimpleOnScale
     @Override
     public boolean onScale(ScaleGestureDetector detector) {
         if(detector.isInProgress()) {
-            if (detector.getCurrentSpan() - detector.getPreviousSpan() > 0) {
+            float currentSpan = detector.getCurrentSpan();
+            float previousSpan = detector.getPreviousSpan();
+            Log.d(TAG, "currentSpan = "+currentSpan+" , previousSpan = "+previousSpan);
+            if (currentSpan - previousSpan > scaleSensitivity)
+            {
                 if (progress < restrictedMaxZoom) {
                     Log.d(TAG, "Zoom IN = " + progress);
                     progress += progressStep;
+                    performZoomOperation((int)progress);
+                    return true;
                 }
-            } else if (detector.getCurrentSpan() - detector.getPreviousSpan() < 0) {
+            }
+            else if (currentSpan - previousSpan < 0)
+            {
                 if (progress > 0) {
                     Log.d(TAG, "Zoom OUT = " + progress);
                     progress -= progressStep;
+                    performZoomOperation((int)progress);
+                    return true;
                 } else {
                     progress = 0;
                 }
             }
-            performZoomOperation((int)progress);
-            return true;
+            return false;
         }
         else{
             return false;
